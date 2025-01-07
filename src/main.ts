62989c057cb45e472637f1f269d93b9cbecea597
Naveedahmedtech <technaveedahmed@gmail.com>
2025-01-07 19:56:49 +0500

    serverless de

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,35 @@
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
-import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
-import { ResponseInterceptor } from './interceptor/response.interceptor';
+import {ValidationPipe, VersioningType} from "@nestjs/common";
+import {PrismaExceptionFilter} from "./filters/prisma-exception.filter";
+import {ResponseInterceptor} from "./interceptor/response.interceptor";
 import * as cookieParser from 'cookie-parser';
+import {createLogger} from "./utils/logger.util";
 import * as express from 'express';
+import { join } from 'path';
 
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-
-  app.enableCors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    logger: createLogger(),
   });
-
+  app.enableCors({
+    origin: process.env.FRONTEND_URL, 
+    credentials: true,  
+});
   app.use(cookieParser());
   app.useGlobalFilters(new PrismaExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
-
+  app.use('/static', express.static(join(__dirname, '..', 'public')));
   app.setGlobalPrefix('api');
   app.enableVersioning({
-    type: VersioningType.URI,
+    type: VersioningType.URI, // URI-based versioning
   });
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  await app.init();
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+  await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
-
-// Export the Express server to be used as a serverless function
-export default server;
