8e09e2dbe2d36a99b17773a32aa1d782078cddab
Naveedahmedtech <technaveedahmed@gmail.com>
2025-01-07 19:46:24 +0500

    serverless

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,43 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import {ValidationPipe, VersioningType} from "@nestjs/common";
-import {PrismaExceptionFilter} from "./filters/prisma-exception.filter";
-import {ResponseInterceptor} from "./interceptor/response.interceptor";
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
+import { ResponseInterceptor } from './interceptor/response.interceptor';
 import * as cookieParser from 'cookie-parser';
-import {createLogger} from "./utils/logger.util";
 import * as express from 'express';
-import { join } from 'path';
 
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: createLogger(),
-  });
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL, 
-    credentials: true,  
-});
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+
   app.use(cookieParser());
   app.useGlobalFilters(new PrismaExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
-  app.use('/static', express.static(join(__dirname, '..', 'public')));
+
   app.setGlobalPrefix('api');
   app.enableVersioning({
-    type: VersioningType.URI, // URI-based versioning
+    type: VersioningType.URI,
   });
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.init();
 }
+
 bootstrap();
+
+// Export the Express server to be used as a serverless function
+export default server;
